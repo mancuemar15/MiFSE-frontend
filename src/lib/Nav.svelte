cd39a88dea1772e9afb598fe63ce8819fa3ad758
mancuemar15 <mancuemar15@gmail.com>
2023-04-23 13:30:33 +0200

    Añadidos Nav y Header

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Router, Route, Link } from "svelte-navigator";
+
+    const opcionesNav = [
+        { pagina: "Inicio", ruta: "/" },
+        { pagina: "Inicio", ruta: "/" },
+        { pagina: "Inicio", ruta: "/" },
+    ];
+</script>
+
+<nav>
+    <ul>
+        {#each opcionesNav as opcionNav}
+            <li>
+                <Link to={opcionNav.ruta}>{opcionNav.pagina}</Link>
+            </li>
+        {/each}
+    </ul>
+</nav>
