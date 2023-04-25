d865de6a612d736d15852621e4a54358dfb24dd9
mancuemar15 <mancuemar15@gmail.com>
2023-04-25 18:04:43 +0200

    Caracteristicas refactorizado por componentes

---
+++ src/lib/Caracteristica.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let titulo;
+    export let icono;
+</script>
+
+<div class="caracteristica-box d-flex align-items-center">
+    <i class={icono} />
+    <h3>{titulo}</h3>
+</div>
+
+<style>
+    .caracteristica-box {
+        padding: 24px 20px;
+        box-shadow: 0px 0 30px rgba(1, 41, 112, 0.08);
+        transition: 0.3s;
+        height: 100%;
+    }
+
+    .caracteristica-box h3 {
+        font-size: 18px;
+        color: #012970;
+        font-weight: 700;
+        margin: 0;
+    }
+
+    .caracteristica-box i {
+        line-height: 0;
+        background: #ecf3ff;
+        padding: 4px;
+        margin-right: 10px;
+        font-size: 24px;
+        border-radius: 3px;
+        transition: 0.3s;
+    }
+
+    .caracteristica-box:hover i {
+        background: #4154f1;
+        color: #fff;
+    }
+</style>
--- src/lib/Caracteristicas.svelte
+++ src/lib/Caracteristicas.svelte
@@ -1,5 +1,15 @@
 <script>
+    import Caracteristica from "./Caracteristica.svelte";
     import imagenCaracteristicas from "../assets/caracteristicas.png";
+
+    const caracteristicas = [
+        { titulo: "Buscar centros", icono: "bi bi-check" },
+        { titulo: "Filtrar centros", icono: "bi bi-check" },
+        { titulo: "Realizar tu lista de preferencias", icono: "bi bi-check" },
+        { titulo: "Consultar últimas posiciones", icono: "bi bi-check" },
+        { titulo: "Contactar con residentes", icono: "bi bi-check" },
+        { titulo: "Leer y escribir opiniones", icono: "bi bi-check" },
+    ];
 </script>
 
 <section id="caracteristicas" class="caracteristicas">
@@ -16,137 +26,16 @@
 
             <div class="col-lg-6 mt-5 mt-lg-0 d-flex">
                 <div class="row align-self-center gy-4">
-                    <div class="col-md-6">
-                        <div
-                            class="caracteristica-box d-flex align-items-center"
-                        >
-                            <i class="bi bi-check" />
-                            <h3>Buscar centros</h3>
+                    {#each caracteristicas as caracteristica}
+                        <div class="col-md-6">
+                            <Caracteristica
+                                titulo={caracteristica.titulo}
+                                icono={caracteristica.icono}
+                            />
                         </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div
-                            class="caracteristica-box d-flex align-items-center"
-                        >
-                            <i class="bi bi-check" />
-                            <h3>Filtrar centros</h3>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div
-                            class="caracteristica-box d-flex align-items-center"
-                        >
-                            <i class="bi bi-check" />
-                            <h3>Realizar tu lista de preferencias</h3>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div
-                            class="caracteristica-box d-flex align-items-center"
-                        >
-                            <i class="bi bi-check" />
-                            <h3>Consultar últimas posiciones</h3>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div
-                            class="caracteristica-box d-flex align-items-center"
-                        >
-                            <i class="bi bi-check" />
-                            <h3>Contactar con residentes</h3>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6">
-                        <div
-                            class="caracteristica-box d-flex align-items-center"
-                        >
-                            <i class="bi bi-check" />
-                            <h3>Leer y escribir opiniones</h3>
-                        </div>
-                    </div>
+                    {/each}
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-<style>
-    .caracteristicas .caracteristica-box {
-        padding: 24px 20px;
-        box-shadow: 0px 0 30px rgba(1, 41, 112, 0.08);
-        transition: 0.3s;
-        height: 100%;
-    }
-
-    .caracteristicas .caracteristica-box h3 {
-        font-size: 18px;
-        color: #012970;
-        font-weight: 700;
-        margin: 0;
-    }
-
-    .caracteristicas .caracteristica-box i {
-        line-height: 0;
-        background: #ecf3ff;
-        padding: 4px;
-        margin-right: 10px;
-        font-size: 24px;
-        border-radius: 3px;
-        transition: 0.3s;
-    }
-
-    .caracteristicas .caracteristica-box:hover i {
-        background: #4154f1;
-        color: #fff;
-    }
-
-    .caracteristicas .feture-tabs h3 {
-        color: #012970;
-        font-weight: 700;
-        font-size: 32px;
-        margin-bottom: 10px;
-    }
-
-    @media (max-width: 768px) {
-        .caracteristicas .feture-tabs h3 {
-            font-size: 28px;
-        }
-    }
-
-    .caracteristicas .feture-tabs .tab-content i {
-        font-size: 24px;
-        line-height: 0;
-        margin-right: 8px;
-        color: #4154f1;
-    }
-
-    .caracteristicas .caracteristica-icons h3 {
-        color: #012970;
-        font-weight: 700;
-        font-size: 32px;
-        margin-bottom: 20px;
-        text-align: center;
-    }
-
-    @media (max-width: 768px) {
-        .caracteristicas .caracteristica-icons h3 {
-            font-size: 28px;
-        }
-    }
-
-    .caracteristicas .caracteristica-icons .content .icon-box i {
-        font-size: 44px;
-        line-height: 44px;
-        color: #0245bc;
-        margin-right: 15px;
-    }
-
-    .caracteristicas .caracteristica-icons .content .icon-box p {
-        font-size: 15px;
-        color: #848484;
-    }
-</style>
