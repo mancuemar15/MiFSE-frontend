63c44d16ac7f6ad53f4a896a0a3b1638fc8ae1cf
mancuemar15 <mancuemar15@gmail.com>
2023-04-30 23:39:10 +0200

    Quitada importación Footer

--- src/lib/Inicio.svelte
+++ src/lib/Inicio.svelte
@@ -3,7 +3,6 @@
     import Caracteristicas from "./Caracteristicas.svelte";
     import Contacto from "./Contacto.svelte";
     import Faqs from "./Faqs.svelte";
-    import Footer from "./Footer.svelte";
     import Valores from "./Valores.svelte";
 </script>
 
