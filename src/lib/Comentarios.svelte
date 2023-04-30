31a73c474f060de504fbd845fcbb1f0c5de55c48
mancuemar15 <mancuemar15@gmail.com>
2023-04-30 10:47:18 +0200

    Añadido usuario en store

--- src/lib/Comentarios.svelte
+++ src/lib/Comentarios.svelte
@@ -1,22 +1,17 @@
 <script>
+    import { usuario } from "./store.js";
     let idCentro;
 </script>
 
-<div class="blog-comments">
+<section class="blog-comments">
     <div class="container">
         <h4 class="comments-count">8 Comments</h4>
 
-        <div id="comment-1" class="comment">
+        <div id="comment-1" class="comment border-bottom">
             <div class="d-flex">
-                <div class="comment-img">
-                    <img src="assets/img/blog/comments-1.jpg" alt="" />
-                </div>
                 <div>
                     <h5>
                         <a href="">Georgia Reader</a>
-                        <a href="#" class="reply"
-                            ><i class="bi bi-reply-fill" /> Reply</a
-                        >
                     </h5>
                     <time datetime="2020-01-01">01 Jan, 2020</time>
                     <p>
@@ -30,17 +25,11 @@
         </div>
         <!-- End comment #1 -->
 
-        <div id="comment-2" class="comment">
+        <div id="comment-2" class="comment border-bottom">
             <div class="d-flex">
-                <div class="comment-img">
-                    <img src="assets/img/blog/comments-2.jpg" alt="" />
-                </div>
                 <div>
                     <h5>
                         <a href="">Aron Alvarado</a>
-                        <a href="#" class="reply"
-                            ><i class="bi bi-reply-fill" /> Reply</a
-                        >
                     </h5>
                     <time datetime="2020-01-01">01 Jan, 2020</time>
                     <p>
@@ -49,79 +38,14 @@
                     </p>
                 </div>
             </div>
-
-            <div id="comment-reply-1" class="comment comment-reply">
-                <div class="d-flex">
-                    <div class="comment-img">
-                        <img src="assets/img/blog/comments-3.jpg" alt="" />
-                    </div>
-                    <div>
-                        <h5>
-                            <a href="">Lynda Small</a>
-                            <a href="#" class="reply"
-                                ><i class="bi bi-reply-fill" /> Reply</a
-                            >
-                        </h5>
-                        <time datetime="2020-01-01">01 Jan, 2020</time>
-                        <p>
-                            Enim ipsa eum fugiat fuga repellat. Commodi quo quo
-                            dicta. Est ullam aspernatur ut vitae quia mollitia
-                            id non. Qui ad quas nostrum rerum sed necessitatibus
-                            aut est. Eum officiis sed repellat maxime vero nisi
-                            natus. Amet nesciunt nesciunt qui illum omnis est et
-                            dolor recusandae. Recusandae sit ad aut impedit et.
-                            Ipsa labore dolor impedit et natus in porro aut.
-                            Magnam qui cum. Illo similique occaecati nihil modi
-                            eligendi. Pariatur distinctio labore omnis incidunt
-                            et illum. Expedita et dignissimos distinctio laborum
-                            minima fugiat. Libero corporis qui. Nam illo odio
-                            beatae enim ducimus. Harum reiciendis error dolorum
-                            non autem quisquam vero rerum neque.
-                        </p>
-                    </div>
-                </div>
-
-                <div id="comment-reply-2" class="comment comment-reply">
-                    <div class="d-flex">
-                        <div class="comment-img">
-                            <img src="assets/img/blog/comments-4.jpg" alt="" />
-                        </div>
-                        <div>
-                            <h5>
-                                <a href="">Sianna Ramsay</a>
-                                <a href="#" class="reply"
-                                    ><i class="bi bi-reply-fill" /> Reply</a
-                                >
-                            </h5>
-                            <time datetime="2020-01-01">01 Jan, 2020</time>
-                            <p>
-                                Et dignissimos impedit nulla et quo distinctio
-                                ex nemo. Omnis quia dolores cupiditate et. Ut
-                                unde qui eligendi sapiente omnis ullam. Placeat
-                                porro est commodi est officiis voluptas repellat
-                                quisquam possimus. Perferendis id consectetur
-                                necessitatibus.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- End comment reply #2-->
-            </div>
-            <!-- End comment reply #1-->
         </div>
         <!-- End comment #2-->
 
-        <div id="comment-3" class="comment">
+        <div id="comment-3" class="comment border-bottom">
             <div class="d-flex">
-                <div class="comment-img">
-                    <img src="assets/img/blog/comments-5.jpg" alt="" />
-                </div>
                 <div>
                     <h5>
                         <a href="">Nolan Davidson</a>
-                        <a href="#" class="reply"
-                            ><i class="bi bi-reply-fill" /> Reply</a
-                        >
                     </h5>
                     <time datetime="2020-01-01">01 Jan, 2020</time>
                     <p>
@@ -139,17 +63,11 @@
         </div>
         <!-- End comment #3 -->
 
-        <div id="comment-4" class="comment">
+        <div id="comment-4" class="comment border-bottom">
             <div class="d-flex">
-                <div class="comment-img">
-                    <img src="assets/img/blog/comments-6.jpg" alt="" />
-                </div>
                 <div>
                     <h5>
                         <a href="">Kay Duggan</a>
-                        <a href="#" class="reply"
-                            ><i class="bi bi-reply-fill" /> Reply</a
-                        >
                     </h5>
                     <time datetime="2020-01-01">01 Jan, 2020</time>
                     <p>
@@ -161,58 +79,31 @@
             </div>
         </div>
         <!-- End comment #4 -->
-
         <div class="reply-form">
-            <h4>Leave a Reply</h4>
-            <p>
-                Your email address will not be published. Required fields are
-                marked *
-            </p>
-            <form action="">
-                <div class="row">
-                    <div class="col-md-6 form-group">
-                        <input
-                            name="name"
-                            type="text"
-                            class="form-control"
-                            placeholder="Your Name*"
-                        />
+            <h4 class="mb-3">Deja tu comentario y valoración</h4>
+            {#if $usuario}
+                <form action="">
+                    <div class="row">
+                        <div class="col form-group">
+                            <textarea
+                                name="comment"
+                                class="form-control"
+                                placeholder="Comentario*"
+                            />
+                        </div>
                     </div>
-                    <div class="col-md-6 form-group">
-                        <input
-                            name="email"
-                            type="text"
-                            class="form-control"
-                            placeholder="Your Email*"
-                        />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col form-group">
-                        <input
-                            name="website"
-                            type="text"
-                            class="form-control"
-                            placeholder="Your Website"
-                        />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col form-group">
-                        <textarea
-                            name="comment"
-                            class="form-control"
-                            placeholder="Your Comment*"
-                        />
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary"
-                    >Post Comment</button
-                >
-            </form>
+                    <button type="submit" class="btn btn-primary"
+                        >Enviar comentario</button
+                    >
+                </form>
+            {:else}
+                <p>Para poder comentar debes iniciar sesión o registrarte.</p>
+                <a href="/login" class="btn btn-primary">Iniciar sesión</a>
+                <a href="/registro" class="btn btn-primary">Registrarse</a>
+            {/if}
         </div>
     </div>
-</div>
+</section>
 
 <style>
     .blog-comments {
@@ -228,20 +119,14 @@
         position: relative;
     }
 
-    .blog-comments .comment .comment-img {
-        margin-right: 14px;
-    }
-
-    .blog-comments .comment .comment-img img {
-        width: 60px;
-    }
-
     .blog-comments .comment h5 {
         font-size: 16px;
         margin-bottom: 2px;
     }
 
     .blog-comments .comment h5 a {
+        font-family: "Nunito", sans-serif !important;
+        text-decoration: none;
         font-weight: bold;
         color: #444444;
         transition: 0.3s;
@@ -251,24 +136,11 @@
         color: #4154f1;
     }
 
-    .blog-comments .comment h5 .reply {
-        padding-left: 10px;
-        color: #012970;
-    }
-
-    .blog-comments .comment h5 .reply i {
-        font-size: 20px;
-    }
-
     .blog-comments .comment time {
         display: block;
         font-size: 14px;
         color: #013ca3;
         margin-bottom: 5px;
-    }
-
-    .blog-comments .comment.comment-reply {
-        padding-left: 40px;
     }
 
     .blog-comments .reply-form {
@@ -284,17 +156,6 @@
 
     .blog-comments .reply-form p {
         font-size: 14px;
-    }
-
-    .blog-comments .reply-form input {
-        border-radius: 4px;
-        padding: 10px 10px;
-        font-size: 14px;
-    }
-
-    .blog-comments .reply-form input:focus {
-        box-shadow: none;
-        border-color: #a0aaf8;
     }
 
     .blog-comments .reply-form textarea {
