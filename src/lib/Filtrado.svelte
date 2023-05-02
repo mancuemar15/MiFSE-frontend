696e3a3d795b75f007fc8b808d2975a9427025c9
mancuemar15 <mancuemar15@gmail.com>
2023-05-02 11:46:36 +0200

    Añadido que Haz tu lista se ponga activo

---
+++ src/lib/Filtrado.svelte
@@ -0,0 +1,37 @@
+<script>
+    import TituloPagina from "./TituloPagina.svelte";
+</script>
+
+<TituloPagina seccion="Lista" titulo="Lista de enfermería" />
+<section id="centro" class="centro">
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-lg-5">
+                <div class="centro-info">
+                    <h3>Información del centro</h3>
+                    <ul>
+                        <li>
+                            <strong>Valoración</strong>
+                        </li>
+                        <li>
+                            <strong>Dirección</strong>
+                        </li>
+                        <li>
+                            <strong>Localidad</strong>
+                        </li>
+                        <li>
+                            <strong>Provincia</strong>
+                        </li>
+                        <li>
+                            <strong>Comunidad Autónoma</strong>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-7 border" />
+        </div>
+    </div>
+</section>
+
+<style>
+</style>
