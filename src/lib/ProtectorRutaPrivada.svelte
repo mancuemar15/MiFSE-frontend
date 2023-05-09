bc6f091349527b8c2ca763e928a712925444ab6c
mancuemar15 <mancuemar15@gmail.com>
2023-05-09 22:19:02 +0200

    Rutas securizadas

---
+++ src/lib/ProtectorRutaPrivada.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { useNavigate, useLocation } from "svelte-navigator";
+    import { usuario } from "./store";
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    $: if (!$usuario) {
+        navigate("/", {
+            state: { from: $location.pathname },
+            replace: true,
+        });
+    }
+</script>
+
+{#if $usuario}
+    <slot />
+{/if}
---
+++ src/lib/RutaPrivada.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Route } from "svelte-navigator";
+    import ProtectorRutaPrivada from "./ProtectorRutaPrivada.svelte";
+
+    export let path;
+</script>
+
+<Route {path} let:params let:location let:navigate>
+    <ProtectorRutaPrivada>
+        <slot {params} {location} {navigate} />
+    </ProtectorRutaPrivada>
+</Route>
