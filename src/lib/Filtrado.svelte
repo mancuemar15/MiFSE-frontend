9aaebbbe0f8dc886e2edcc478b99ca47c6d852b3
mancuemar15 <mancuemar15@gmail.com>
2023-05-09 22:40:28 +0200

    Arreglado botón filtrado que desaparecía cuando disabled

--- src/lib/Filtrado.svelte
+++ src/lib/Filtrado.svelte
@@ -394,4 +394,8 @@
         background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='rgba%280, 0, 0, 0.25%29'/%3e%3c/svg%3e") !important;
         border: 1px solid rgba(0, 0, 0, 0.25) !important;
     }
+
+    button:disabled {
+        background-color: #4154f1 !important;
+    }
 </style>
